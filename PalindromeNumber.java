abb1458a49732f3ef70e6fea1458d360b74028a3
Laxman Anand <laxman7291@gmail.com>
2024-10-22 14:29:35 +0530

    first commit

---
+++ PalindromeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int reversedNum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        // Check if the original number is equal to the reversed number
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
